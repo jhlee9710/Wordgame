ac88a245218af766a9f5860bf648a2b7baf96730
jhlee9710 <jhlee9710@gmail.com>
2022-10-12 17:21:17 +0900

    word game code

---
+++ src/com/java/WordQuiz/Word.java
@@ -0,0 +1,15 @@
+package com.java.WordQuiz;
+
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Word {
+	private String english;
+	private String korean;
+
+}
